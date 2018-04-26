4fd14cffe60a960ae22f90a191d44f5c66a96be1
Hope <hkim@oscars.org>
2018-04-26 14:16:53 -0700

    Trying to figure it out...

--- 15_func-exercises/06_euler-two_heads/main.go
+++ 15_func-exercises/06_euler-two_heads/main.go
@@ -1,8 +1,36 @@
 package main
 
 import "fmt"
+import "math/rand"
+
+func loop() int {
+	//f := 0
+	//s := 0
+	var f int
+	var s int
+	var i int
+
+	for {
+		i++
+		if rand.Intn(2) == 0 {
+			f = i
+			for {
+				i++
+				if rand.Intn(2) == 0 && i + 1 == f {
+					s = i
+					return i
+				} else if rand.Intn(2) == 0 && i + 1 != f {
+					i = 0
+					loop()
+				}
+			}
+		}
+	}
+}
 
 func main() {
+	tosses := loop()
+	fmt.Println(tosses / 10^18)
 	// Q(a/b, p) = q
 	// a≡bq(modp)
 	// Q(3/5,109)
@@ -28,16 +56,19 @@ func main() {
 	//	}
 	//}
 
-	q2 := 1;
+	//q2 := 1;
+	//
+	//for {
+	//	a := 9 % 109
+	//	b := (31 * q2) % 109
+	//	if a == b {
+	//		fmt.Println(q2, "is q, the smallest positive number")
+	//		return
+	//	} else {
+	//		q2++
+	//	}
+	//}
 
-	for {
-		a := 9 % 109
-		b := (31 * q2) % 109
-		if a == b {
-			fmt.Println(q2, "is q, the smallest positive number")
-			return
-		} else {
-			q2++
-		}
-	}
+	// 0 is heads, 1 is tails
+
 }
